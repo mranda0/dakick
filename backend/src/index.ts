5b627402930d6ac11639b2220602d520340fba49
Miranda0503 <pieromg2004@gmail.com>
2026-06-03 22:57:51 -0500

    fix: cors for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 4000
 
 app.use(helmet())
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: ["http://localhost:3000", "https://dakick.vercel.app"],
   credentials: true
 }))
 app.use(express.json())
